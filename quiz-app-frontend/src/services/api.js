c254da23bca0ad466314b11ca5d5e29ec2a5a362
Vishvam16 <202303057@dau.ac.in>
2026-07-06 01:42:25 +0530

    Update API URL for production

--- quiz-app-frontend/src/services/api.js
+++ quiz-app-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://quizapp-1fe2.onrender.com",
     headers: {
         "Content-Type": "application/json"
     }
